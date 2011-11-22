07f71e7c2092f372909888207b480fc60ef34846
Rockosov <rockosov@gmail.com>
2011-11-22 22:46:57 +0300

    Added: Permutation class(direct and inverse order); Eval delta_a and delta_c

--- diff_analyze/blocks.py
+++ diff_analyze/blocks.py
@@ -20,11 +20,28 @@ class Block ( object ):
 			raise ValueError, "Error of input num!"
 
 		self.num = num
+
+		# значение функции блоков на множестве определения [0;15]
+		self.a_output = []
+		for i in range( 16 ):
+			self.a_output.append( self.substitution( i ) )
+
+		self.diff_table = []
+
 	def print_block( self ):
 		print "Blocks", self.num, "[", self.var_num, "]:"
 		for i in self.content:
-			for j in range(len(i)):
-				stdout.write(str(i[j]) + " ")
+			for j in i:
+				stdout.write(str(j) + " ")
+			print
+
+	def print_diff_table( self ):
+		if self.diff_table == []:
+			raise ValueError, "diff_table is empty!"
+		print "diff_table of block %d:" % self.num
+		for i in self.diff_table:
+			for j in i:
+				print "%02d" % j ,
 			print
 	
 	def substitution( self, bits ):
@@ -33,3 +50,57 @@ class Block ( object ):
 			return self.content[(bits & 1) + ((bits & 8) >> 2)][(bits & 6) >> 1]
 		else:
 			return self.content[bits >> 3][bits & 7]
+
+	def build_diff_table( self ):
+		# подготовим место
+		self.diff_table.extend( range( 16 ) )
+		for i in range( len( self.diff_table ) ):
+			self.diff_table[ i ] = range( 16 )
+			for j in range( len( self.diff_table[ i ] ) ):
+				# и инициализируем как нужно
+				self.diff_table[ i ][ j ] = 0
+		for delta_a in range( 16 ):
+			for a1 in range( 16 ):
+				a2 = delta_a ^ a1
+				# посчитаем c1 и c2
+				c1 = self.substitution( a1 )
+				c2 = self.substitution( a2 )
+				# вычислим delta_c
+				delta_c = c1 ^ c2
+				# запомним
+				self.diff_table[delta_a][delta_c] += 1
+
+	def max_in_diff_table( self ):
+		result = list()
+		if self.diff_table == []:
+			raise ValueError, "diff_table is empty!"
+		for i in self.diff_table[1:]:
+			maximum = max( i )
+			for j in range( len( i ) ):
+				if maximum == i[ j ]:
+					result.append( ( self.diff_table.index( i ), j, i[ j ] ) )
+		temp = max( result, key = lambda current: current[2] )
+		out = list()
+		for index, value in enumerate( result ):
+			if value[ 2 ] == temp[ 2 ]:
+				out.append( value )
+		return out
+
+class Permutation ( object ):
+	def __init__( self, initial_name ):
+		if initial_name == "E":
+			self.direct = conf.PERM_EXT
+		elif initial_name == "P":
+			self.direct = conf.PERM
+		else:
+			raise ValueError, "Unknown name of permutation"
+		self.inverse = self.__inverse( self.direct )
+
+	def __inverse( self, direct):
+		result = range( max( direct ) )
+		for i in range( len( result ) ):
+			result[i] = []
+		for i in range( len( direct ) ):
+			result[ direct[ i ] - 1 ].append(i + 1)
+		return result
+
--- diff_analyze/search_characteristic.py
+++ diff_analyze/search_characteristic.py
@@ -1,21 +1,61 @@
 import sys
-from blocks import Block
+from blocks import *
 
 __author__ = "rockosov@gmail.com"
 
 def main():
+	
 	f_block = Block( 1 )
 	s_block = Block( 2 ) 
 	t_block = Block( 3 )
 
+	print "Print substitution blocks:"
 	f_block.print_block()
 	s_block.print_block()
 	t_block.print_block()
+	print
 
-	example = 10
-	print f_block.substitution( example )
-	print s_block.substitution( example )
-	print t_block.substitution( example )
+	print "Print A dependence of substitution blocks:"
+	print "a_output of 1 block:", f_block.a_output
+	print "a_output of 2 block:", s_block.a_output
+	print "a_output of 3 block:", t_block.a_output
+	print
+
+	print "Buid and print diff_table:"
+	f_block.build_diff_table()
+	f_block.print_diff_table()
+	s_block.build_diff_table()
+	s_block.print_diff_table()
+	t_block.build_diff_table()
+	t_block.print_diff_table()
+	print
+
+	print "Eval and print maximum of diff_tables:"
+	f_block_max = list()
+	s_block_max = list()
+	t_block_max = list()
+	f_block_max = f_block.max_in_diff_table()
+	print "First block Maximum:", f_block_max
+	s_block_max = s_block.max_in_diff_table()
+	print "Second block Maximum:", s_block_max
+	t_block_max = t_block.max_in_diff_table()
+	print "Third block Maximum:", t_block_max
+	print
+
+	print"Build permutation blocks:"
+	E_perm = Permutation( "E" )
+	P_perm = Permutation( "P" )
+	print "E-permutation:"
+	print "DIRECT"
+	print E_perm.direct
+	print "INVERSE"
+	print E_perm.inverse
+	print "P-permutation:"
+	print "DIRECT"
+	print P_perm.direct
+	print "INVERSE"
+	print P_perm.inverse
+	print
 
 if __name__ == "__main__":
 	sys.exit(main())
