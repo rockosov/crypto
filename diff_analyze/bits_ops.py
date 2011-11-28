6eb8715eba7ce5e24070b34c7a2a3d3e9bd82d16
Rockosov <rockosov@gmail.com>
2011-11-29 01:31:04 +0300

    Added get_bits(); fixed set_bit()

--- diff_analyze/bits_ops.py
+++ diff_analyze/bits_ops.py
@@ -6,4 +6,18 @@ def get_bit( source, position ):
 	return ( source >> position ) & 1
 
 def set_bit( destination, position, bit ):
-	return destination | ( bit << position )
+	if bit == 1:
+		return destination | ( bit << position )
+	else:
+		mask = 0
+		for i in range( len( bin( destination ).lstrip( "0b" ) ) ):
+			if i == position:
+				continue
+			mask += 1 << i
+		return destination & mask 
+
+def get_bits( source, position, num ):
+	mask = 0
+	for i in range( num ):
+		mask += 1 << i
+	return ( source >> position ) & mask
